64440ea4fc410a9f92161b56fd2bcb21c4d18fd8
leaf-kit <>
2026-04-09 03:05:11 +0900

    Remove arch restriction from formula

--- Formula/playgraph.rb
+++ Formula/playgraph.rb
@@ -4,12 +4,8 @@ class Playgraph < Formula
   version "0.1.0"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/leaf-kit/playgraph.md/releases/download/v0.1.0/playgraph-0.1.0-x86_64-darwin.tar.gz"
-      sha256 "afdb2bfb8c8e2f696d327f4c70cd8cf9d0c6180eb6aeace2fac8fa830b128a1b"
-    end
-  end
+  url "https://github.com/leaf-kit/playgraph.md/releases/download/v0.1.0/playgraph-0.1.0-x86_64-darwin.tar.gz"
+  sha256 "afdb2bfb8c8e2f696d327f4c70cd8cf9d0c6180eb6aeace2fac8fa830b128a1b"
 
   def install
     bin.install "playgraph"
